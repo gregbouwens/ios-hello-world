2e486c5484e12eb27567ea155ad9322e93d6b56f
Greg Bouwens <greg.bouwens@outlook.com>
2025-12-17 18:02:19 -0800

    Initial Commit

---
+++ Envelope/EnvelopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvelopeApp.swift
+//  Envelope
+//
+//  Created by rentamac on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct EnvelopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
